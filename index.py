3818165cc663cedf2616de3eae9ee43c73817ef0
lewisj2501 <jones.lewis2501@gmail.com>
2022-04-06 15:57:56 +0100

    - Added nltk functionality (e.g. polarity score, stopwords etc.). - Added string cleanup functionality (e.g. getting rid of punctuation in tweets) - Table displaying tweet object has been updated with a new sentiment header.

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 # import Twitter API, access tokens and datetime for console log
 from flask import Flask, redirect, url_for, render_template, request
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import string
 import tweepy as twitter
 import keys
 import time, datetime
@@ -29,15 +33,57 @@ def twitterData(game):
     # append user input with base query
     query = game + ' ' +baseQuery
     # finds recent tweets based on search query, default 10 max
-    response = client.search_recent_tweets(query=query, tweet_fields=['created_at', 'lang'])
+    response = client.search_recent_tweets(query=query,tweet_fields=['created_at', 'lang'],max_results=100)
+    # datalist holds a list containing lists of tweet objects
     datalist = []
     for tweet in response.data:
-        sublist = []
-        sublist.append(tweet.id)
-        sublist.append(tweet.text)
-        sublist.append(tweet.lang)
-        sublist.append(tweet.created_at)
-        datalist.append(sublist)
+        # filters for tweets in engilsh
+        if tweet.lang == "en":
+            # each tweet object is added to this list
+            sublist = []
+            # after tokenising the tweet text, add the words (tokens) to this list
+            finalTweet = []
+            # string that holds the sentiment of a tweet
+            tweet_polarity = ""
+            # cleansing process of the text
+            tweet_text = tweet.text
+            # converts tweet text to all lowercase
+            tweet_text_lower = tweet_text.lower()
+            # removes punctuation
+            cleaned_tweet = tweet_text_lower.translate(str.maketrans('', '', string.punctuation))
+            # nltk function that tokenises tweet text (breaks into words)
+            tokenised_tweet = word_tokenize(cleaned_tweet,"english")
+            # removes english stop words (e.g. I, so, a, our, etc. ) and appends to finalTweet list
+            for token in tokenised_tweet:
+                if token not in stopwords.words('english'):
+                    finalTweet.append(token)
+            # converts finalTweet list (all the valuble words in the tweet) back to a string
+            cleaned_tokenised_tweet = ' '.join(map(str,finalTweet))
+            # uses nltk function to get polarity score (is the tweet neg, neu, or pos)
+            tweet_polarity_score = SentimentIntensityAnalyzer().polarity_scores(cleaned_tokenised_tweet)
+            # get dict neg value
+            neg = tweet_polarity_score['neg']
+            # get dict pos value
+            pos = tweet_polarity_score['pos']
+            # if the polarity is more negative than postive, determine tweet as negative, elif its postive,
+            # else its neutral
+            if neg > pos:
+                tweet_polarity = "Negative Tweet"
+            elif pos > neg:
+                tweet_polarity = "Positive Tweet"
+            else:
+                tweet_polarity = "Neutral Tweet"
+            # add tweet object properties to list that will be displayed on web page
+            sublist.append(tweet.id)
+            sublist.append(tweet.text)
+            sublist.append(tweet.lang)
+            sublist.append(tweet.created_at)
+            sublist.append(tweet_polarity)
+            # add list of tweet object to the list of other tweet objects
+            datalist.append(sublist)
+        else:
+            # dont do anything if not english
+            print("/n")
 
     return datalist
 # runs website, runs it in debug mode (don't have to refresh page each time)
